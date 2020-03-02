a533802081a681f7a77397f6c4500f5ae32433f4
samuel <osamaimafidon@gmail.com>
2020-03-02 16:37:46 +0100

    ft(plugged in routes): Plugged in routes to Login and signup components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Switch>
         <Route exact path="/">Home</Route>
-        <Route path="/register">Register</Route>
-        <Route path="/login">Login</Route>
+        <Route path="/register" component={SignUp}></Route>
+        <Route path="/login" component={SignIn}></Route>
         <Route path="/game">Game</Route>
       </Switch>
     </div>
