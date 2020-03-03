64efff7115589f3e3213a3f972d836d5cba49885
Anna Winther <anski.anna@gmail.com>
2020-03-03 16:43:40 +0000

    Import styled-components and add a game component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+import Game from './components/Game';
 
 function App() {
   return (
-    <div className="container">
+    <div className="app">
+      <Navigation/>
       <Switch>
-        <Route exact path="/">Home</Route>
+        <Route exact path="/" component={Home}></Route>
         <Route path="/register" component={SignUp}></Route>
         <Route path="/login" component={SignIn}></Route>
-        <Route path="/game">Game</Route>
+        <Route path="/game"><Game/></Route>
       </Switch>
     </div>
   );
