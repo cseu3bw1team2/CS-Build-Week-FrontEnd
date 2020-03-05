f75f65526346f6252387dc4dcebdb02f5a89382e
Anna Winther <anski.anna@gmail.com>
2020-03-05 17:28:07 +0000

    Style game page

--- src/components/Canvas/Game.js
+++ src/components/Canvas/Game.js
@@ -202,25 +202,59 @@ class Game extends Component {
   
   render() {
     return (
+      // <div>
+      //   <canvas className="canvas" ref="canvas" width={1150} height={700} />
+      //   <div className="btn-container">
+      //     <div className="north">
+      //       <button onClick={() => this.movePlayer("n")}>North</button>
+      //     </div>
+      //     <div className="left-right">
+      //       <div>
+      //         <button onClick={() => this.movePlayer("w")}>West</button>
+      //       </div>
+      //       <div>
+      //         <button onClick={() => this.movePlayer("e")}>East</button>
+      //       </div>
+      //     </div>
+      //     <div className="south">
+      //       <button onClick={() => this.movePlayer("s")}>South</button>
+      //     </div>
+      //   </div>
+      // </div>
       <div>
-        <canvas className="canvas" ref="canvas" width={1080} height={450} />
-        <div className="btn-container">
-          <div className="north">
-            <button onClick={() => this.movePlayer("n")}>North</button>
-          </div>
-          <div className="left-right">
-            <div>
-              <button onClick={() => this.movePlayer("w")}>West</button>
+      {/* <h1>Hello {gameData.name}</h1> */}
+       {/* <div className="card card-cascade white"> */}
+          {/* <div className="view view-cascade overlay"> */}
+            <canvas className="canvas" ref="canvas" width={1150} height={600} />
+          {/* </div> */}
+          <div className="controller">
+          <div className="text">
+              <h4 className="card-title"><strong>Your position</strong></h4>
+              <h6 className="font-weight-bold indigo-text py-2">Room: </h6>
+              <p className="font-weight-bold black-text">Description:</p>
+              <p className="card-text">Where do you want to move next?<br></br>Use your keys or hit the buttons to move to the next room</p>
             </div>
-            <div>
-              <button onClick={() => this.movePlayer("e")}>East</button>
+              {/* <div className="card-body card-body-cascade text-right"></div> */}
+                <div className="btn-container">
+                    <div className="north">
+                      <button className="button brown" onClick={() => this.movePlayer("n")}>North</button>
+                    </div>
+                    <div className="left-right">
+                      <div>
+                        <button className="button brown" onClick={() => this.movePlayer("w")}>West</button>
+                      </div>
+                      <div>
+                        <button className="button brown" onClick={() => this.movePlayer("e")}>East</button>
+                      </div>
+                    </div>
+                    <div className="south">
+                      <button className="button brown" onClick={() => this.movePlayer("s")}>South</button>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="south">
-            <button onClick={() => this.movePlayer("s")}>South</button>
-          </div>
-        </div>
-      </div>
+         </div>
+      // {/* </div> */}
+    // </div>
     );
   }
 }
